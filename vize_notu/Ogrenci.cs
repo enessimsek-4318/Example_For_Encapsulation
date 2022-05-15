cfb28b259784d25f7a35a1bc27ce34e17a050512
enessimsek-4318 <101976848+enessimsek-4318@users.noreply.github.com>
2022-05-15 11:22:26 +0300

    Example_For_Encapsulation

---
+++ vize_notu/Ogrenci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vize_notu
+{
+    public class Ogrenci
+    {
+        private int vize_1;
+        public int vize
+        {
+            get
+            {
+                return vize_1;
+            }
+            set
+            {
+                if (value <= 100 && value >= 0)
+                {
+                    vize_1 = value;
+                }
+                else
+                {
+                    Console.WriteLine("Girmiş Olduğunuz Vize Notunuz 0-100 aralığında değildir.");
+                }
+            }
+        }
+    }
+}
---
+++ vize_notu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vize_notu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Lütfen Vize Notunuzu Giriniz:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.vize = a;
+            Console.WriteLine("Vize Notunuz:"+ogrenci.vize);
+            Console.ReadLine();
+            
+        }
+    }
+}
